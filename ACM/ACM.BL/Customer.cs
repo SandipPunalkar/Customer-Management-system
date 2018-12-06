8a1082bddccf155841eb7fa9fa1b11aaa5ece5a6
Sandip <sandippunalkar9@gmail.com>
2018-12-06 12:12:41 +0530

    Implemented customer class

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -4,5 +4,28 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ",";
+                    }
+
+                    fullName += FirstName;
+                }
+
+                return fullName;
+            }
+        }
     }
 }
