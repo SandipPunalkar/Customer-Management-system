7cc2faf24cd7e3e6abe3b2530b405246e9545798
Sandip <sandippunalkar9@gmail.com>
2018-12-06 12:35:53 +0530

    Validate method is test in test Customer test class

--- ACM/ACM.BLTest/CustomerTest.cs
+++ ACM/ACM.BLTest/CustomerTest.cs
@@ -56,5 +56,19 @@ namespace ACM.BLTest
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            var customer = new Customer();
+            customer.LastName = "Jobs";
+            customer.EmailAddress = "stevejobs@gmail.com";
+
+            var expected = true;
+
+            var actual = customer.Validate();
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
