c6deb45d64bad0dc8ecaccef798cf0695865009c
Sandip <sandippunalkar9@gmail.com>
2018-12-06 12:20:53 +0530

    Implement customerTest class

--- ACM/ACM.BLTest/CustomerTest.cs
+++ ACM/ACM.BLTest/CustomerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ACM.BL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ACM.BLTest
@@ -7,8 +8,53 @@ namespace ACM.BLTest
     public class CustomerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FullNameTestValid()
         {
+            //Arrange
+            var customer = new Customer();
+            customer.FirstName = "Sandip";
+            customer.LastName = "Patil";
+
+            string expected = "Patil,Sandip";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //Arrange
+            var customer = new Customer();
+            customer.LastName = "Patil";
+
+            string expected = "Patil";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //Arrange
+            var customer = new Customer();
+            customer.FirstName = "Sandip";
+
+            string expected = "Sandip";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
