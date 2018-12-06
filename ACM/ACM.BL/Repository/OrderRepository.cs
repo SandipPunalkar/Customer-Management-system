e5c8090e9687517163a82fae4ea30763c0aee3f2
Sandip <sandippunalkar9@gmail.com>
2018-12-06 13:33:40 +0530

    Create OrderRepository class

---
+++ ACM/ACM.BL/Repository/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ACM.BL.Repository
+{
+    public class OrderRepository
+    {
+        public Order Retrieve(int orderId)
+        {
+            Order order = new Order(orderId);
+            if (orderId == 10)
+            {
+                order.OrderDate = new DateTimeOffset(2014, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+            return order;
+        }
+
+        public bool Save()
+        {
+            // Code that saves the defined order
+            return true;
+        }
+
+    }
+}
