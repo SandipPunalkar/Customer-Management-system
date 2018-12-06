ec6d1e53fb2da2e34e8ce8c504833e85b074811e
Sandip <sandippunalkar9@gmail.com>
2018-12-06 13:17:45 +0530

    Create customerRepositoty class

---
+++ ACM/ACM.BL/Repository/CustomerRepositry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL.Repository
+{
+    public class CustomerRepositry
+    {
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+            }
+            return customer;
+        }
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
